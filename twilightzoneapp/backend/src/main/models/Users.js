ff2d05f078c0d05973d7273ccc14722c3bb3895a
Jessica Woods <48572600+JessicaWoods03@users.noreply.github.com>
2022-01-31 16:13:50 -0500

    fixed Users with email
    added email to users and mongo models

--- twilightzoneapp/backend/src/main/models/Users.js
+++ twilightzoneapp/backend/src/main/models/Users.js
@@ -23,6 +23,13 @@ let UserSchema = new mongoose.Schema( {
         required: true,
         trim: true
     }
+    //adding changes to user login Jan 31st, 2022, added changes to Mongo as well
+    email: {
+        type: String,
+        required: true,
+        unique: true
+
+    }
 
 });
 module.exports = mongoose.model('Users', UserSchema, "users");
